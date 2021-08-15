9ebfab691e9ed25c64d316269733ae8bd528d0a5
Muhammad Huzaifa Khan <huzaifa.khan@tpsonline.com>
2021-08-15 17:07:00 +0500

    Body Parser

--- app.js
+++ app.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware
+app.use(bodyParser.json());
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
-// api/v1
-app.get(`${api}/`,(req,res)=>{
-    res.send('Server Started.');
+// get data
+app.get(`${api}/products`,(req,res)=>{
+    const product1 = {
+        id:1,
+        name:'Hair Dresser',
+        image:'some_url'
+
+    }
+    res.send(product1);
+    // res.send('Server Started.');
+})
+// post data
+app.post(`${api}/products`,(req,res)=>{
+    const newProduct = req.body;
+    console.log(newProduct);
+    res.send(newProduct);
+    // res.send('Server Started.');
 })
 
 app.listen(3000,() => {
