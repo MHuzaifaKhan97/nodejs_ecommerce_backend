9940c9f3db4c41c2faf1d71f62a607ab9a1d7a87
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-08-16 23:21:58 +0500

    Add files via upload

--- app.js
+++ app.js
@@ -8,10 +8,31 @@ const mongoose = require('mongoose');
 app.use(bodyParser.json());
 app.use(morgan('combined'));
 
+// Product Schema
+const productSchema = mongoose.Schema({
+    name: String,
+    image: String,
+    countInStock: Number,
+});
+
+// Create Modal
+const Product = mongoose.model('Product', productSchema);
+
 // For Public Variables
 require('dotenv/config');
 
 const api = process.env.API_URL;
+
+// post data
+app.post(`${api}/products`, (req, res) => {
+
+    const product = new Product({
+        name: req.body.name,
+        image: req.body.image,
+        countInStock: req.body.countInStock
+    });
+    product.save().then((createdProduct)=> res.send(createdProduct));
+})
 
 // get data
 app.get(`${api}/products`, (req, res) => {
@@ -24,17 +45,12 @@ app.get(`${api}/products`, (req, res) => {
     res.send(product1);
     // res.send('Server Started.');
 })
-// post data
-app.post(`${api}/products`, (req, res) => {
-    const newProduct = req.body;
-    console.log(newProduct);
-    res.send(newProduct);
-    // res.send('Server Started.');
-})
 
-mongoose.connect(process.env.CONNECTION_STRING, 
-    { useUnifiedTopology: true, 
-        useNewUrlParser: true, 
+
+mongoose.connect(process.env.CONNECTION_STRING,
+    {
+        useUnifiedTopology: true,
+        useNewUrlParser: true,
     })
     .then(() => console.log('Database Connection is Ready'))
     .catch((err) => console.log(err))
