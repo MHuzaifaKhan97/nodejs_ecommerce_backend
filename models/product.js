61c5f97952dcecf8a8558772b1fc2bec0b4d27b7
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-08-22 15:03:16 +0500

    Product Model & Schema

--- models/product.js
+++ models/product.js
@@ -2,9 +2,62 @@ const mongoose = require('mongoose');
 
 // Product Schema
 const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: {type: Number, required: true},
+    name: {
+        type: String,
+        required: true 
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    richDescription:{
+        type:String,
+        default:''
+    },
+    image:{
+        type:String,
+        default:''
+    },
+    images:[{type:String}],
+    brand:{
+        type:String,
+        default:''
+    },
+    price:{
+        type: Number, 
+        default:0
+    },
+    category:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref: 'Category',
+        required:true
+    },
+    countInStock:{
+        type: Number,
+        required: true,
+        min:0,
+        max:255
+    },
+    rating:{
+        type: Number,
+        required: true,
+        min:0,
+        max:5
+    },
+    numReviews:{
+        type: Number,
+        required: true,
+      
+    },
+    isFeatured:{
+        type: Boolean,
+        default: false,
+    },
+    dateCreated:{
+        type: Date,
+        default: Date.now,
+    },
+
 });
 
 
