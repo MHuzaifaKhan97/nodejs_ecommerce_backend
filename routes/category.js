ce446571ddfe96d6df1f91cb94bf0867e3eed843
Muhammad Huzaifa Khan <47402181+MHuzaifaKhan97@users.noreply.github.com>
2021-08-28 21:09:32 +0500

    Adding Categories & Getting list of Categories

--- routes/category.js
+++ routes/category.js
@@ -1,34 +1,39 @@
-const {Category}  = require('../models/category');
+const Category = require('../models/category');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', async (req,res)=>{
+router.get('/', async (req, res) => {
     const categoryList = await Category.find();
 
-    if(!categoryList){
-        res.status(500).json({success: false});
+    if (!categoryList) {
+        res.status(500).json({ success: false });
     }
     res.send(categoryList);
 })
 
 router.post('/', async (req, res) => {
+
+    if(req.body.name === null || req.body.name === "" || req.body.name === undefined){
+        return res.status(400).send({
+            success: false,
+            message:'Name is required'
+        })
+    }
     let category = new Category({
         name: req.body.name,
         icon: req.body.icon,
         color: req.body.color,
     });
+
     category = await category.save();
     if (!category) {
-        return res.status(404).send({
-            message: "The category can not be created",
-            success: false,
-        })
+        return res.status(404).send({ success: false, message: 'Category can not be created' });
     }
-
     res.send({
-        message: "The category is successfully created",
-        success: true
-    });
-})
+        success: true,
+        message: 'Category is successfully created'
+    })
+
+});
 
 module.exports = router;
